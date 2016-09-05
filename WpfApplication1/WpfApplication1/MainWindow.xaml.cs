989556ce6ce52bb4dc405130612920fb5ed44907
Makoto Uwaya <makoto.uwaya@gmail.com>
2016-09-06 00:52:32 +0900

    WPFプロジェクトを追加
    パフォーマンス測定検証用プロジェクトを追加
    readmeファイルに参考URLを記述

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var items = new List<ListItem>();
+            items.Add(new ListItem() { Name = "1-First" });
+            items.Add(new ListItem() { Name = "2-Second" });
+            items.Add(new ListItem() { Name = "3-Third" });
+            lvSample.ItemsSource = items; 
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# ＷＰＦ パフォーマンス測定プロジェクト
+
+## はじめに
+これは、パフォーマンスに関する基準値を測定するためのプロジェクトです。
+
+## 参考URL
+アプリケーションのタイムライン
+https://msdn.microsoft.com/ja-jp/library/dn957934.aspx
+
+UIの応答性の分析
+https://msdn.microsoft.com/ja-jp/library/dn194502.aspx
+
+WPFのチューニングまとめ
+http://qiita.com/Koki_jp/items/f6ef5bbb2da9be44744c
